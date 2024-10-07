12ac44c600e4113c4e2ae25bfd6b71b8b5c07bce
Dizellpo <161682020+Dizellpo@users.noreply.github.com>
2024-10-07 12:54:48 +0500

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,15 @@
+import 'normalize.css'
 import '../scss/style.scss'
+// import 'swiper/swiper-bundle.min.css'
 
-console.log('Works!')
+import initSliders from './slider.js'
+import initModals from './modal.js'
+import initBurger from './burger.js'
+import initReadMoreBtn from './read-more.js'
+
+window.addEventListener('DOMContentLoaded', function () {
+  initBurger()
+  initSliders()
+  initModals()
+  initReadMoreBtn()
+})
